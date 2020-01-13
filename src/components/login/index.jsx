87da94d3919d17dc6c4dc3a73727b868d1a15e19
fz <1792576194@qq.com>
2020-01-13 16:35:59 +0800

    '完成表单校验'

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -9,7 +9,25 @@ import './index.less';
 @Form.create()
 class Login extends Component {
 
- 
+ validator = (rule,value,callback) => {
+
+
+    const name = rule.field ==='username' ? '用户名' : '密码';
+  const reg = /^\w+$/;
+
+  if(!value){
+    callback(`${name}不能为空`);
+  }else if(value.length<4){
+    callback(`${name}必须大于四位数`);
+  }else if(value.length>15){
+    callback(`${name}必须十五位数`);
+  }else if(!reg.test(value)){
+    callback(`${name}只能包字母，数字，下划线`);
+  }
+
+
+   callback();
+ }
   render() {
     const { getFieldDecorator } = this.props.form;
 
@@ -27,22 +45,26 @@ class Login extends Component {
                'username',
                {
                  rules: [
-                   {
-                     required:true,
-                     massage:'用户名不能为空'
-                   },
-                   {
-                      min:4,
-                      massage:'用户名必须大于四位数'
-                   },
-                   {
-                      max:15,
-                      massage:'用户名必须小于15位'
-                   },
-                   {
-                    pattern:/^\w$/,
-                    massage:'用户名只能包字母，数字，下划线'
-                   }
+                  //  {
+                  //    required:true,
+                  //    message:'用户名不能为空'
+                  //  },
+                  //  {
+                  //     min:4,
+                  //     message:'用户名必须大于四位数'
+                  //  },
+                  //  {
+                  //     max:15,
+                  //     message:'用户名必须小于15位'
+                  //  },
+                  //  {
+                  //   pattern:/^\w+$/,
+                  //   message:'用户名只能包字母，数字，下划线'
+                  //  }
+                  {
+                    validator:this.validator
+                  }
+                  
                  ]
                }
              )(
@@ -55,12 +77,24 @@ class Login extends Component {
              )}
             </Form.Item>
             <Form.Item>
-              <Input 
-              prefix={
-              <Icon type='lock' style={{color: 'rgba(0,0,0,.25)' }} />
-            }
-             placeholder="密码"
-             />
+             {getFieldDecorator(
+               'password',
+                {
+                  rules:[
+                    {
+                      validator:this.validator
+                    }
+                  ]
+                }
+
+             )(
+                <Input 
+                prefix={
+                <Icon type='lock' style={{color: 'rgba(0,0,0,.25)' }} />
+              }
+               placeholder="密码"
+               />
+             )}
             </Form.Item>
             <Form.Item>
             <Button className="login-form-btn" type='primary'>登录</Button>
